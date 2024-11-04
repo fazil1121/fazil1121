c5917f50d9d6223d1c28bd2aa4a4144b08e05b7a
fazil1121 <faziil.career@gmail.com>
2024-11-04 07:40:56 -0500

    Add files via upload

---
+++ src/config/froster.js
@@ -0,0 +1,34 @@
+import {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} from "@google/generative-ai";
+
+const apiKey = import.meta.env.GEMINI_API_KEY;
+const genAI = new GoogleGenerativeAI("AIzaSyDDZQ6EreE6In6T6cCYrwtv6YkQkidOI7Q");
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.0-pro",
+});
+
+const generationConfig = {
+  temperature: 0.9,
+  topP: 1,
+  maxOutputTokens: 2048,
+  responseMimeType: "text/plain",
+};
+
+async function run(prompt) {
+  const chatSession = model.startChat({
+    generationConfig,
+    history: [
+    ],
+  });
+
+  const result = await chatSession.sendMessage(prompt);
+  const response = result.response;
+  console.log(result.response.text());
+  return response.text();
+}
+
+export default run;
